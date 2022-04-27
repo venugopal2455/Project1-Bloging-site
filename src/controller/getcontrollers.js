0577cee4db5b95aebbb689d81787a42e13f63a56
unknown <venu2455@gmail.com>
2022-04-27 21:20:07 +0530

    getapis

--- src/controller/getcontrollers.js
+++ src/controller/getcontrollers.js
@@ -2,47 +2,25 @@ const blogModel=require("../model/blog")
 const AuthorModel=require('../model/author')
 const mongoose = require("mongoose")
 const Object_id = mongoose.Types.ObjectId
-const getAllBlogs=async function(req,res){
-    try{  
-        const details=await blogModel.find({$and:[{isDeleted:true},{isPublished:true}]}).select()
-       
-        res.status(200).send({msg:details, status: true })
+
+
+   const getFilterBlog = async function (req, res) {
+        try {
+             let data=req.query
+             console.log(data)
+             let getData =await blogModel.find( {$and : [data, { isDeleted: false }, { isPublished: true }]})
+             console.log( [data, { isDeleted: false }, { isPublished: true }])
+            if(!(data))
+        getData = await blogModel.find({ isDeleted: false, isPublished: true})
+            
+            if (getData.length===0)
+                return res.status(404).send({ status: false, msg: "Blogs not present" })
+            res.status(200).send({ status: true, msg: getData })
         }
-    catch(err){
-        console.log(err)
-                res.status(404).send({msg:err.message})
-                }
+        catch (error) {
+            res.status(400).send({ status: false, msg: error.message })
+        }
     }
-const getfilterblog=async function(req,res){
-  try{  
-        const authorId=req.query.authorId
-        const category=req.query.category
-        const tag=req.query.tag
-        const subcategory=req.query.subcategory
-        if(!authorId)return res.status(400).send({msg:"please enter the authorId"})
-        // if(!tag)return res.status(400).send({msg:"please enter the tag"})
-        if(!category)return res.status(400).send({msg:"please enter the category"})
-        // if(!subcategory)return res.status(400).send({msg:"please enter the subcategory"})
-        if(authorId&&category){
-                    const filterAuthor=AuthorModel.find(authorId)
-                    const id =filterAuthor[0].authorId
-                    const filtercategory=blogModel.find({authorId:id})
-                    res.status(200).send({msg:filtercategory})
-        }
-        
-        //     const filterAuthor=AuthorModel.find().populate(authorId)
-        // const filtercategory=blogModel.find(category)
-        // const filtertags=blogModel.find(tag)
-        // const filtersubcategory=blogModel.find(subcategory)
-
-
-// res.send()
-}
-catch(err){
-res.status(500).send({msg:err.message})
-}
-   }
    
-   
-   module.exports.getAllBlogs=getAllBlogs
-   module.exports.getfilterblog=getfilterblog
+module.exports.getFilterBlog=getFilterBlog
+  
